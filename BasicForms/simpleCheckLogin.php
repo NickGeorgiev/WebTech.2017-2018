84302663b6da6496d970162b1c9e665c25b52689
Nikola Georgiev <nikikg95@gmail.com>
2017-11-17 18:03:45 +0200

    added simple version for homework

---
+++ BasicForms/simpleCheckLogin.php
@@ -0,0 +1,22 @@
+<?php
+$errors = array();
+if(strlen($_POST['courseName']) > 150 ){
+  $errors['courseName'] = "<p> Името на курса е по-дълго от 15 символа. </p><br />\n";
+}
+if(strlen($_POST['lecturer'])>200){
+  $errors['lecturer'] = "<p> Името на преподавателя е по-дълго от 200 символа. </p><br />\n";
+}
+if(strlen($_POST['courseInfo'])<10){
+  $errors['courseInfo'] = "<p> Описанието на курса е по-малко от 10 символа. </p><br />\n";
+}
+if($_POST['credits'] < 0){
+  $errors['credits'] = "<p> Броят кредити не е положително число.  </p><br />\n";
+}
+if(!empty($errors)){
+  echo "<p>Следните невалидни данни са въведени:</p>\n";
+  foreach ($errors as $key => $value) {
+    echo $value;
+  }
+}
+else echo "<p> Данните са коректни! </p><br />\n";
+ ?>
